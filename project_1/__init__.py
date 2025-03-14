512d0e241460290cea252690435252b0d53045b4
Caleb C <wolfertamer@gmail.com>
2025-03-13 17:22:50 -0700

    Added comments to every file, explaining basic functionality and adding some TODOs

--- project_1/__init__.py
+++ project_1/__init__.py
@@ -6,11 +6,28 @@ __version__ = "0.0.1"
 import jwt
 
 import secrets
+# makes it so that every session automatically expires after 30 minutes
 app.permanent_session_lifetime = timedelta(minutes=30)
+# generates a random 256 bit token to use a secret key
+# this makes it so that the key changes every time the server is run
+# to make it more convenient, we may switch this to use a config.py file with a premade token
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
 
+
+# user object used to store data. Can be imported into other files. 
+# FORMAT:
+# username: {'email':'email_value', 'password':'password_value'}
 users = {"coolguy":{'email':'coolguy@gmail.com','password':'coolp@ssword'}}
 
+# TODO:
+# Create an inventory object for storing inventory information.
+# Each inventory object must be associated with a username
+# Other values TBD
+
+
+# token_required decorator that checks for a JWT in the headers
+# Uses the format of Authorization: Bearer xxx.yyy.zzz
+# Token expiry is 30 minutes and is handled automatically by jwt.decode()
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -29,12 +46,15 @@ def token_required(f):
         try:
             # decoding the payload to fetch the stored details
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            # check that the user exists and is associated with the current session
             if data['username'] in users and session['user'] == data['username']:
                 current_user = data['username']
+            # return 401 if not pased
             else:
                 return jsonify({
                     'error' : 'Token is invalid'
                 }), 401
+        # end the session if the token has expired, return 401
         except jwt.ExpiredSignatureError:
             session.pop('user', None)
             return jsonify({
@@ -52,5 +72,5 @@ def token_required(f):
 if __name__ == '__main__': 
     app.run()
 
-
+# import at the end to prevent an import loop
 import project_1.views
--- project_1/blueprints/test_blueprint/__init__.py
+++ project_1/blueprints/test_blueprint/__init__.py
@@ -4,7 +4,8 @@ from project_1 import users, token_required
 
 test_blueprint = Blueprint('test_blueprint', __name__)
 
-
+# uses @token-required to check for a valid token. returns a list of users if there is one.
+# No actual functionality, just used for testing token validity.
 @test_blueprint.route('/')
 @token_required
 def test_token(current_user):
